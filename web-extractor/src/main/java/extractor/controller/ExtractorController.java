9b05e9d8e6111bc2492ec397a180a7d2f823c5a6
david.rusycki <davidrusyck@gmail.com>
2023-10-01 20:46:31 -0300

    feat: add test endpoint

--- web-extractor/src/main/java/extractor/controller/ExtractorController.java
+++ web-extractor/src/main/java/extractor/controller/ExtractorController.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import extractor.entity.ComprasNetEntity;
 import extractor.enums.OrganEnum;
+import extractor.service.db.ComprasNetService;
 import extractor.service.strategy.ExtractorServiceContext;
 
 @RestController
@@ -15,6 +17,9 @@ public class ExtractorController {
 
 	@Autowired
 	private ExtractorServiceContext context;
+	
+	@Autowired
+	private ComprasNetService service;
 	
 	@GetMapping("/health")
 	public String health() {
@@ -31,6 +36,29 @@ public class ExtractorController {
 		}
 		
 		return "sucess!";
+	}
+	
+	@GetMapping("/test")
+	public ComprasNetEntity teste() {
+		
+		var entity = new ComprasNetEntity();
+		
+		// ARRANGE
+		entity.setOrgao("orgao teste do david");
+		entity.setUasg(123456);
+		entity.setModalidade("modalidade do david");
+		entity.setObjeto("construção de api para licitações");
+		entity.setDataEdital("a data é amanhã");
+		entity.setEndereco("rua jão gomez da silva");
+		entity.setTelefone("(47) 40028922");
+		entity.setFax("(134) Não sei como é um fax");
+		entity.setDataEntregaProposta("amanha até de tarde");
+		entity.setUrlEdital("https://localhost:8080/amanhaedia");
+		
+		// ACT
+		entity = service.insert(entity);
+				
+		return entity;
 	}
 	
 }
